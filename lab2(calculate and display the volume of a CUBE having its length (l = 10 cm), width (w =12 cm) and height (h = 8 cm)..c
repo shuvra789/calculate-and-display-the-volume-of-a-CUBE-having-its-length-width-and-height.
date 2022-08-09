b44bbb9e5eef9014f05fdb8077fb6369c6056c65
Shuvra Saha Rimjim <110848482+shuvra789@users.noreply.github.com>
2022-08-09 10:52:43 +0600

    Add files via upload

---
+++ lab2(calculate and display the volume of a CUBE having its length (l = 10 cm), width (w =12 cm) and height (h = 8 cm)..c
@@ -0,0 +1,14 @@
+//Write a Program to calculate and display the volume of a CUBE having its length (l = 10 cm), width (w =12 cm) and height (h = 8 cm).
+
+#include<stdio.h>
+int main(void)
+{
+    int l, w, h, vol;
+    l =10;
+    w =12;
+    h =8;
+    vol =l*w*h;
+    printf("The Volume of the cube is: %d", vol);
+
+    return 0;
+}
